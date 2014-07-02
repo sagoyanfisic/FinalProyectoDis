13fbf2a0e640e2bb4624c9e71f8a697fb7d26a2e
sagoyanfisic <sagoyanfisic1@gmail.com>
2014-07-02 12:48:34 -0500

    incluyendo vlenv

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sagoyanfisic/python-dev/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.5.2','django-admin.py'
+__requires__ = 'Django==1.5.2'
+import pkg_resources
+pkg_resources.run_script('Django==1.5.2', 'django-admin.py')
